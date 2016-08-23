7002f8c74279905db5fcb276dcc3264ccb116e9b
Jksalas <jksalas1514@gmail.com>
2016-08-22 23:29:21 -0600

    Add files via upload

---
+++ debouncer.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:11:56 08/21/2016 
+// Design Name: 
+// Module Name:    debouncer 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+ module debouncer(
+	input clk,
+	input [2:0] sw,
+	output [2:0] sw_out,
+	input wire reset
+    );
+
+   reg [2:0] register1,register2,register3;
+
+   always @ (posedge clk)
+      if (reset == 1) begin
+         register1 <= 3'b000;
+			register2 <= 3'b000;
+			register3 <= 3'b000;
+		end
+      else begin
+         register1 <= {register1[1:0], sw[0]};
+			register2 <= {register2[1:0], sw[1]};
+			register3 <= {register3[1:0], sw[2]};
+		end
+			
+	//and final que decide cuando se le da el valor de 1 logico al pulsador
+   assign sw_out[0] = register1[0] & register1[1] & register1[2];
+	assign sw_out[1] = register2[0] & register2[1] & register2[2];
+	assign sw_out[2] = register3[0] & register3[1] & register3[2];
+	
+
+endmodule
---
+++ testVGA.v
@@ -0,0 +1,89 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:04:59 08/21/2016 
+// Design Name: 
+// Module Name:    testVGA 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module testVGA;
+
+	// Inputs
+	reg clk;
+	reg reset;
+	reg [2:0] rgbswitches;
+
+	// Outputs
+	wire [2:0] rgbtext;
+	wire hsync;
+	wire vsync;
+	wire [9:0] pixel_x, pixel_y;
+
+	// Instantiate the Unit Under Test (UUT)
+	TopVGA uut (
+		.clk(clk), 
+		.reset(reset), 
+		.rgbswitches(rgbswitches), 
+		.rgbtext(rgbtext), 
+		.hsync(hsync), 
+		.vsync(vsync),
+		.pixel_x(pixel_x),
+		.pixel_y(pixel_y)
+		);
+		
+	integer i;
+	integer j;
+	
+	initial
+	begin
+		clk = 0;
+	forever
+		begin
+		 #10 clk = ~clk; 
+		end
+	end
+	
+		
+	
+	initial
+		begin
+			clk = 0;
+			reset = 1;
+			j=0;
+			#50
+			reset = 0; 
+			
+			rgbswitches[0] = 0;
+			rgbswitches[1] = 0;
+			rgbswitches[2] = 1;
+			
+			//archivo txt para observar los bits, simulando una pantalla
+			i= $fopen("Test1.txt","w");
+			for(j=0;j<27200;j=j+1) begin
+				#40
+				if( (pixel_x > 1'b0) & (pixel_x < 10'b1100011111))
+					$fwrite(i,"%d",rgbtext[2]);
+				else
+					$fwrite(i,"\n");
+			end
+				
+			
+			#16800000
+			reset=0;
+			$fclose(i);
+			$stop;
+	end
+      
+endmodule
